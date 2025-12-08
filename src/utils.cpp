346fa0652f6d9e6be91496bbc4dea49c22e60b28
Yifan LI <liyifan0117@gmail.com>
2025-12-08 14:41:14 +0100

    add the part of using the bin from python

--- src/utils.cpp
+++ src/utils.cpp
@@ -12,10 +12,12 @@ Tensor softmax(const Tensor& x) {
     const float* x_ptr =x.fptr();
     float* y_ptr = y.fptr();
 
-    float max_val=x_ptr[0];
-    for (int i=0;i<n-1;i++) {
-        if (max_val<x_ptr[i]) {max_val=x_ptr[i];}
-    }
+    float max_val = x_ptr[0];
+    for (int i = 1; i < n; ++i) {
+        if (max_val < x_ptr[i]) {
+            max_val = x_ptr[i];
+        }
+	}
     float sum = 0.0f;
     for (int i=0;i<n;i++) {
          float val =std::exp(x_ptr[i] - max_val);
@@ -37,8 +39,9 @@ Tensor gelu(const Tensor& x) {
 
     for (int i=0;i<n;i++) {
         float v=x_ptr[i];
-        float t=std::tanh(0.7988456f*(v+0.044715f*v*v*v));
-        y_ptr[i]=0.5f*v*(1.0f+t);
+        const float kAlpha = 0.7978845608f;  // sqrt(2/pi)
+        float t = std::tanh(kAlpha * (v + 0.044715f * v * v * v));
+        y_ptr[i] = 0.5f * v * (1.0f + t);
     }
     return y;
 }
